f2bdbf361f8fe131e768bc9a7daec6cce128cba4
Daniel van den Eijkel <1515314+aische@users.noreply.github.com>
2026-05-14 12:52:35 +0200

    load tools

--- src/LLM/Generate/LoadModels.hs
+++ src/LLM/Generate/LoadModels.hs
@@ -12,10 +12,18 @@ import Data.Maybe (catMaybes)
 import Data.Text (Text)
 import Data.Text qualified as T
 import GHC.Generics (Generic)
-import LLM (ChatEnv (..), claudeGateway, geminiGateway, noHooks, ollamaGateway, openAIGateway)
-import LLM.Core.Types (LLMGateway)
+import GHC.RTS.Flags (TraceFlags (user))
+import LLM (ChatEnv (..), Tool (toolDef), ToolDef (toolName), claudeGateway, geminiGateway, noHooks, ollamaGateway, openAIGateway, toTool)
+import LLM.Core.Types (LLMGateway, Tool)
 import LLM.Core.Usage (PricingInfo (..))
 import LLM.Generate.Types (ModelConfig (..))
+import LLM.Tools.Age (ageTool)
+import LLM.Tools.FsConfig (FsConfig, mkFsConfig)
+import LLM.Tools.Readdir (readdirToolTyped)
+import LLM.Tools.Readfile (readfileToolTyped)
+import LLM.Tools.ReplaceInFile (replaceInFileToolTyped)
+import LLM.Tools.Weather (weatherToolTyped)
+import LLM.Tools.Writefile (writefileToolTyped)
 import System.Environment (lookupEnv)
 
 modelCatalogFilePath :: FilePath
@@ -92,31 +100,35 @@ createModelConfig gatewayMap mci = case Map.lookup (providerName mci) gatewayMap
         }
   Nothing -> Left $ "Provider not found: " ++ show (providerName mci)
 
-loadChatEnvs :: FilePath -> ModelConfigMap -> ExceptT String IO ChatEnvMap
-loadChatEnvs filePath modelConfigMap = do
+loadChatEnvs :: FilePath -> ModelConfigMap -> ToolMap -> ExceptT String IO ChatEnvMap
+loadChatEnvs filePath modelConfigMap toolMap = do
   chatEnvCatalogItems <- ExceptT $ eitherDecodeFileStrict filePath
-  liftEither $ getChatEnvConfigs modelConfigMap chatEnvCatalogItems
+  liftEither $ getChatEnvConfigs modelConfigMap toolMap chatEnvCatalogItems
 
-getChatEnvConfigs :: ModelConfigMap -> [ChatEnvConfigItem] -> Either String ChatEnvMap
-getChatEnvConfigs modelConfigMap chatEnvCatalogItems = Map.fromList <$> configs
+getChatEnvConfigs :: ModelConfigMap -> ToolMap -> [ChatEnvConfigItem] -> Either String ChatEnvMap
+getChatEnvConfigs modelConfigMap toolMap chatEnvCatalogItems = Map.fromList <$> configs
   where
     configs = forM chatEnvCatalogItems $ \ceci -> do
-      ce <- createChatEnv modelConfigMap ceci
+      ce <- createChatEnv modelConfigMap toolMap ceci
       pure (chatEnvName ceci, ce)
 
-createChatEnv :: ModelConfigMap -> ChatEnvConfigItem -> Either String ChatEnv
-createChatEnv models conf = do
+createChatEnv :: ModelConfigMap -> ToolMap -> ChatEnvConfigItem -> Either String ChatEnv
+createChatEnv models toolMap conf = do
   let getModel name = case Map.lookup name models of
         Just mc -> Right mc
         Nothing -> Left $ "Model config not found: " ++ show name
+      getTool name = case Map.lookup name toolMap of
+        Just t -> Right t
+        Nothing -> Left $ "Tool config not found: " ++ show name
   modelConfig <- getModel (model conf)
   fb <- mapM getModel (fallbacks conf)
+  tools <- mapM getTool (tools conf)
   pure $
     ChatEnv
       { envModel = modelConfig,
         envFallbacks = fb,
         envSystem = Nothing,
-        envTools = [], -- tools ceci,
+        envTools = tools,
         envMaxToolRounds = maximumToolRounds conf,
         envContextWindow = contextWindowSize conf,
         envHooks = noHooks,
@@ -128,5 +140,34 @@ loadEnvs :: IO (Either String (ChatEnvMap, ModelConfigMap, GatewayMap))
 loadEnvs = runExceptT $ do
   gateways <- liftIO loadGateways
   modelConfigs <- loadModelConfigs modelCatalogFilePath gateways
-  chatEnvs <- loadChatEnvs chatEnvCatalogFilePath modelConfigs
+  toolMap <- liftIO loadTools
+  chatEnvs <- loadChatEnvs chatEnvCatalogFilePath modelConfigs toolMap
   pure (chatEnvs, modelConfigs, gateways)
+
+type ToolMap = Map Text Tool
+
+loadTools :: IO ToolMap
+loadTools = do
+  userProjectPath <- lookupEnv "USER_PROJECT_PATH"
+  fsConfig <- case userProjectPath of
+    Nothing -> pure Nothing
+    Just p -> Just <$> mkFsConfig p
+  pure $ getTools fsConfig
+
+getTools :: Maybe FsConfig -> ToolMap
+getTools fsConfig =
+  let fsTools =
+        maybe
+          []
+          ( \fsConfig ->
+              [ toTool (readfileToolTyped fsConfig),
+                toTool (writefileToolTyped fsConfig),
+                toTool (readdirToolTyped fsConfig),
+                toTool (replaceInFileToolTyped fsConfig)
+              ]
+          )
+      otherTools =
+        [ toTool weatherToolTyped,
+          ageTool
+        ]
+   in Map.fromList $ map (\t -> (toolName $ toolDef t, t)) (fsTools fsConfig ++ otherTools)
